30dc40fa54e623c57eb6fa16ac07945c06e66051
Ruan de Menezes Costa <ruan.cocito@gmail.com>
2017-02-13 22:07:07 -0200

    fix header exposure issue

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token");
+    res.header("Access-Control-Expose-Headers", "X-Access-Token");
     next();
 });
 
