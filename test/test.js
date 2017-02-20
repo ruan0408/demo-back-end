68a5dbd0714e173e7860267a2eea6d7fbd103311
Ruan Costa <ruan.cocito@gmail.com>
2017-02-20 09:27:14 -0300

    adds sample test

--- test/test.js
+++ test/test.js
@@ -10,13 +10,14 @@ describe('GET /api/users', function () {
            .get('/api/users')
            .expect('Content-Type', /json/)
            .expect(200)
-           // .expect(body, function () {
-           //     if (!('username' in body[0])) throw new Error("missing username key");
-           // })
-           .end(function(err, res) {
-               if (err) return done(err);
-               // console.log(res.body);
-               done();
-           });
+           .expect(function (res) {
+               if (!('username' in res.body[0])) throw new Error("missing username key");
+           })
+           .expect(done);
+           // .end(function(err, res) {
+           //     if (err) return done(err);
+           //     // console.log(res.body);
+           //     done();
+           // });
    });
 });
